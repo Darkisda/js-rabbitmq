c7a0933b63fa4718b232e0ba5d4f8f441b08fbe9
Darkisda <luandesouzanascimento2011@gmail.com>
2024-02-27 11:46:18 -0300

    publish-subscribe

--- logger.mjs
+++ logger.mjs
@@ -3,13 +3,12 @@ import * as amqp from 'amqplib';
 const conn = await amqp.connect("amqp://localhost:5672")
 const ch = await conn.createChannel();
 
-await ch.prefetch(1);
 const exchange = await ch.assertExchange('logs', 'fanout', {durable: false})
-const queue = await ch.assertQueue('', {durable: true})
+const queue = await ch.assertQueue('', { exclusive: true })
 await ch.bindQueue(queue.queue, exchange.exchange, '')
 console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue.queue);
 ch.consume(queue.queue, (msg) => {
-    console.log(" [x] Received %s", msg.content.toString());
+    console.log(" [x] Log - %s", msg.content.toString());
     setTimeout(function() {
       console.log(" [x] Done");
       ch.ack(msg)
--- publish.mjs
+++ publish.mjs
@@ -3,12 +3,15 @@ import * as amqp from 'amqplib';
 const conn = await amqp.connect("amqp://localhost:5672")
 const ch = await conn.createChannel();
 
-const queue = await ch.assertQueue('exemple', {durable: true})
-
+await ch.assertExchange('logs', 'fanout', {durable: false})
 for (let index = 1; index <= 10; index++) {
     
-    ch.sendToQueue(queue.queue, Buffer.from(index.toString()), {
+    const send = ch.publish('logs', '', Buffer.from(index.toString()), {
         persistent: true
     })
-    console.log(" [x] Sent '%s'", index);
+    if (send) {
+        console.log(" [x] Sent '%s'", index);
+    }
 }
+// await conn.close()
+// process.exit(0)
